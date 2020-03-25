a39ed925d51ec268d7802fe7dcba66538c6bce30
Sean Selzer <seanselzer@googlemail.com>
2020-03-25 11:31:30 +0000

    plot_stencil_v1.0

--- plot_stencil.py
+++ plot_stencil.py
@@ -6,3 +6,41 @@ Created on Wed Mar 25 10:09:49 2020
 @author: seanselzer
 """
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+def my_plotter(ax, data1, data2, param_dict):
+    """
+    A helper function to make a graph
+
+    Parameters
+    ----------
+    ax : Axes
+        The axes to draw to
+
+    data1 : array
+       The x data
+
+    data2 : array
+       The y data
+
+    param_dict : dict
+       Dictionary of kwargs to pass to ax.plot
+
+    Returns
+    -------
+    out : list
+        list of artists added
+    """
+    out = ax.plot(data1, data2, **param_dict)
+    return out
+
+# which you would then use as:
+#
+data1, data2, data3, data4 = np.random.randn(4, 100)
+fig, ax = plt.subplots(1, 1)
+my_plotter(ax, data1, data2, {'linestyle': '',
+                              'marker': 'o',
+                              'markerfacecolor' : 'teal',
+                              'markeredgecolor' : 'black',
+                              'label':'Data1'})
